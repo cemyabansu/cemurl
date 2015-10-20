875c686c915b46b88c48cd9664ea04423d54e852
cemyabansu <cemyabansu@hotmail.com>
2015-10-21 00:03:43 +0300

    better - nodeJs

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var path = require('path');
+var compression = require('compression');
+
 var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(compression());
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.status(200);
+    res.render('pages/index', {
+        title: 'willkommen',
+        description: 'There is no such thing as perfect',
+        keywords: 'cemyabansu, cem, yabansu, cemyabansu.com'
+    });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('*', function (req, res) {
+    res.status(404);
+    res.render('pages/error', {
+        title: 'What are you looking for is not here.',
+        description: 'error page',
+        keywords: 'error, 404, lol'
+    });
 });
+
+var port = process.env.PORT || 8080;
+app.listen(port);
 
 
