7edf871b7e2d0fa7efabce9915a03124280ac8be
Cem Yabansu <cemyabansu@hotmail.com>
2016-11-25 08:58:25 +0200

    Trying to allow cross-origin

--- controllers/api.js
+++ controllers/api.js
@@ -19,6 +19,13 @@ exports.AddUrl = function (req,res) {
         url : newUrl
     });
 
+    // Allowing Cross-origin
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
     // console.log('new url : ' + newUrl);
     // console.log('new key : ' + newKey);
 
